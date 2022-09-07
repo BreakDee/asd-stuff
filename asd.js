e7f6f0aa7c417d71928307041a3d10ade3c08ab3
Not Ili <99596096+BreakDee@users.noreply.github.com>
2022-09-07 10:46:45 +0200

    Update asd.js

--- asd.js
+++ asd.js
@@ -1,25 +1,28 @@
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+  }
+
+function openurl() {
+  url = makeid(8)
+  window.open("https://www.bing.com/search?q=" + url + "&PC=U316&FORM=CHROMN", "_blank");
+  print(url)
+}
+
 let amount = prompt("Amount", "Amount");
 let text;
 
 if (amount == null || amount == "") {
   amount = 0;
 } else {
-  function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-   return result;
-  }
-
-  function openurl() {
-    url = makeid(8)
-    window.open("https://www.bing.com/search?q=" + url + "&PC=U316&FORM=CHROMN", "_blank");
-    print(url)
-    }
+  
   for (let i = 0; i < amount; i++) {
     openurl()
+    
   }
 }
